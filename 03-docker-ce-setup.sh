243bc247080d6c2a71c3aa6a0ad8224e8fb5a80a
Deepen Dhulla <deepen@deependhulla.com>
2023-04-11 15:18:00 +0530

    update on Tuesday 11 April 2023 03:18:00 PM IST

--- 03-docker-ce-setup.sh
+++ 03-docker-ce-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get install ca-certificates curl gnupg
+apt-get -y remove docker docker-engine docker.io containerd runc
+apt-get -y install ca-certificates curl gnupg
 mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="amd64" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bullseye stable"  >  /etc/apt/sources.list.d/docker.list
